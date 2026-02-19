8adfb08fd6d68f407324bd7031edf77b24f27392
kristine-barr <kristinebarruzo@gmail.com>
2026-02-19 10:26:42 -0800

    Update app_user.fk.sql and app_user.table.sql
    app_user.table.sql
    - replace username with two columns: first_name and last_name
    
    app_user.fk.sql
    - remove ON UPDATE CASCADE

--- schema/app_user.table.sql
+++ schema/app_user.table.sql
@@ -3,7 +3,8 @@
 
 CREATE TABLE IF NOT EXISTS app_user (
     user_id SERIAL PRIMARY KEY, 
-    username VARCHAR(50) NOT NULL UNIQUE, 
+    first_name VARCHAR(50) NOT NULL, 
+    last_name VARCHAR(50) NOT NULL, 
     email VARCHAR(255) NOT NULL UNIQUE,  
     role_id INT NOT NULL
 );
