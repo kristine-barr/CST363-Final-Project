79c8596643538ec496b6ae821bc2df99f299adc0
Bryan Cairo <97150722+dajokerz@users.noreply.github.com>
2026-02-20 20:48:45 -0800

    Update book_genre_keys.sql

--- schema/book_genre_keys.sql
+++ schema/book_genre_keys.sql
@@ -4,10 +4,10 @@
 ALTER TABLE book_genre
 ADD PRIMARY KEY (book_id, genre_id); --Adding Primary Key
 
-ADD CONSTRAINT fk_book --Foreign key for the Book ID
+ADD CONSTRAINT fk_book_genre_book_id --Foreign key for the Book ID
 FOREIGN KEY (book_id)
 REFERENCES book(book_id),
 
-ADD CONSTRAINT fk_genre --Foreign key for the Genre ID
+ADD CONSTRAINT fk_book_genre_genre_id --Foreign key for the Genre ID
 FOREIGN KEY (genre_id)
 REFERENCES genre(genre_id);
